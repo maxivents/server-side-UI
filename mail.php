444744d98186153e39dae96e1db1d6251a0a11d1
mraethel <mraethel@outlook.com>
2018-09-23 14:33:04 +0200

    pre-release-version

--- mail.php
+++ mail.php
@@ -1,12 +1,17 @@
 <?php
-	$email = htmlspecialchars((string) json_decode(file_get_contents('php://input'), true)['email']);
-	$massage = htmlspecialchars((string) json_decode(file_get_contents('php://input'), true)['massage']);
-	
+	$email = htmlspecialchars(json_decode(file_get_contents('php://input'), true)['email']);
+	$massage = htmlspecialchars(json_decode(file_get_contents('php://input'), true)['massage']);
+
+	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+		$email='';
+		$massage='';
+	}
+
 	header('Access-Control-Allow-Headers: Content-Type');
 	header('Content-Type: text/html; charset=utf-8');
 	header('Access-Control-Allow-Origin: *');
 
-	if ($email != '' && $massage != '') {
+	if($email != '' && $massage != ''){
 		mail('info@maxivents.com', 'Anfrage von ' . $email, $massage, 'From: ' . $email);
 	
 		echo '<div style="padding: 10px; border-radius: 10px; background-color: #6bd425;">Ihre Nachricht wurde versendet.<div>';
